6adb7c0888c35fe2ac0deea58ccd2d34e27c4cdd
amdreallyfast <amdreallyfast@hotmail.com>
2014-05-15 13:42:09 -0700

    did some reorganization and testing
    I wanted to test how long servicing the TCPIP stack took.  This is
    critical information for managing cpu resources in another project.
    
    As part of the testing, I decided that it would be a good idea to
    reorganize my code and make good use of my function queue.

--- wifi_basic_TCPIP_demo.X/main.c
+++ wifi_basic_TCPIP_demo.X/main.c
@@ -5,10 +5,10 @@
 
 #include <stdio.h>
 
-//#include "my_TCPIP_framework.h"
+#include "my_TCPIP_framework.h"
 
-//#include "../my_framework/my_C_I2C_handler.h"
-//#include "../my_framework/my_function_queue.h"
+#include "../../github_my_MX4cK_framework/my_C_I2C_handler.h"
+#include "../../github_my_MX4cK_framework/my_function_queue.h"
 
 
 
@@ -77,6 +77,90 @@ void delayMS(unsigned int milliseconds)
 // declaration and when a maximum buffer size needs to be specified.
 #define MESSAGE_BUFFER_SIZE_BYTES 64
 
+void blink_onboard_LED_1(void)
+{
+   if (0 == (gMillisecondsInOperation % 50))
+   {
+      PORTToggleBits(IOPORT_B, BIT_10);
+   }
+}
+
+void my_TCPIP_receive_and_reply(void)
+{
+   int byte_count = 0;
+   char cls_message[CLS_LINE_SIZE + 1];
+   unsigned char rx_buffer[MESSAGE_BUFFER_SIZE_BYTES];
+   unsigned char tx_buffer[MESSAGE_BUFFER_SIZE_BYTES];
+
+   // I like to blink different LEDs depending on whether there is a
+   // connection or not, and I can get away with it because I am only using
+   // a single socket-port
+   // Note: Blinking LEDs would be a poor way to check the connection status
+   // of a multiple socket-port setup.
+   if (0 == (gMillisecondsInOperation % 50))
+   {
+      if (1 == TCPIP_is_there_a_connection_on_port(SERVER_PORT))
+      {
+         PORTClearBits(IOPORT_B, BIT_12);
+         PORTToggleBits(IOPORT_B, BIT_13);
+      }
+      else
+      {
+         PORTClearBits(IOPORT_B, BIT_13);
+         PORTToggleBits(IOPORT_B, BIT_12);
+      }
+   }
+
+   byte_count = TCPIP_bytes_in_RX_FIFO(SERVER_PORT);
+   if (byte_count > 0)
+   {
+      myI2CWriteToLine(I2C2, "data Data DATA!", 1);
+      myI2CWriteToLine(I2C2, "bats Bats BATS!", 2);
+
+      // missige for you, sire
+      snprintf(cls_message, CLS_LINE_SIZE, "RX bytes: %d", byte_count);
+      myI2CWriteToLine(I2C2, cls_message, 1);
+
+      // clear out the message buffer, get the missige, then display it on
+      // the CLS pmod, line 2 (because I want "missige sire:" to display on
+      // the top line)
+      // Note: If the message is longer than a single CLS line, that is ok.
+      // It will just start overwriting the last character until the string
+      // is finished.
+      memset(rx_buffer, 0, MESSAGE_BUFFER_SIZE_BYTES);
+      byte_count = TCPIP_basic_receive(SERVER_PORT, rx_buffer, MESSAGE_BUFFER_SIZE_BYTES);
+      if (byte_count > CLS_LINE_SIZE)
+      {
+         // ??do something if it is too long to print on a single CLS line? no? do we care??
+      }
+      else if (byte_count == MESSAGE_BUFFER_SIZE_BYTES)
+      {
+         // do something?
+      }
+
+      // forcefully null terminate the receive buffer to make sure that
+      // formated string (%s) procede without incident
+      // Note: Chop off the last two bytes, which are the return carriage
+      // and new line characters that Tera Term sends when the return key is
+      // pressed.
+      // Note: If not using Tera Term, do not chop off the last two bytes
+      // unless you know that the sending program appends unwanted
+      // characters or other bytes to the end.
+      //rx_buffer[byte_count - 2] = 0;
+      rx_buffer[byte_count] = 0;
+      snprintf(cls_message, CLS_LINE_SIZE, "%s", rx_buffer);
+      myI2CWriteToLine(I2C2, cls_message, 2);
+
+      // now spit the message back out, formatted to appear visually
+      // different from the sent message
+      // Note: Cross your fingers and hope that the incoming message wasn't
+      // so big that the new format will be truncated in the transmit
+      // buffer.
+      memset(tx_buffer, 0, MESSAGE_BUFFER_SIZE_BYTES);
+      snprintf(tx_buffer, MESSAGE_BUFFER_SIZE_BYTES, "\r\n--You sent: '%s'\r\n", rx_buffer);
+      TCPIP_basic_send(SERVER_PORT, tx_buffer, strlen(tx_buffer));
+   }
+}
 
 // -----------------------------------------------------------------------------
 //                    Main
@@ -92,7 +176,17 @@ int main(void)
    char cls_message[CLS_LINE_SIZE + 1];
    unsigned char rx_buffer[MESSAGE_BUFFER_SIZE_BYTES];
    unsigned char tx_buffer[MESSAGE_BUFFER_SIZE_BYTES];
-   
+
+   unsigned int start_ms = 0;
+   unsigned int end_ms = 0;
+   unsigned int diff_ms;
+   unsigned int diff_count = 0;
+   unsigned int sum_diff_ms = 0;
+   unsigned int avg_diff_ms = 0;
+   unsigned int min_diff_ms = 1000;
+   unsigned int max_diff_ms = 0;
+   unsigned int ms_count_at_max = 0;
+
    gMillisecondsInOperation = 0;
    g_TCPIP_service_can_start = FALSE;
 
@@ -129,7 +223,7 @@ int main(void)
       // when the TCPIP stack will start connecting to the network, and that
       // takes ~30 seconds, during which you should expect to not see this
       // light blinking.
-      PORTToggleBits(IOPORT_B, BIT_10);
+      add_function_to_queue(blink_onboard_LED_1);
 
       // make sure to execute the function queue because the TCPIP servicing
       // function, which is what is responsible for connecting to a network, is
@@ -146,15 +240,11 @@ int main(void)
       }
       snprintf(cls_message, CLS_LINE_SIZE, "%d.%d.%d.%d", ip_1, ip_2, ip_3, ip_4);
       myI2CWriteToLine(I2C2, cls_message, 2);
-
-      // delay for a little bit so that the LED doesn't blink too fast to see
-      delayMS(50);
+      
    }
 
    // open a port on the connected network, and display the result (success or
    // fail) along with the new IP address
-   // Note: The new IP address may be shorter, character-wise, than its default
-   // address, so clear the CLS line before displaying the new one.
    generic_ret_val = TCPIP_open_socket(SERVER_PORT);
    if (generic_ret_val < 0)
    {
@@ -168,8 +258,10 @@ int main(void)
       snprintf(cls_message, CLS_LINE_SIZE, "port %d open on", SERVER_PORT);
    }
    myI2CWriteToLine(I2C2, cls_message, 1);
-   
+
    // now the new IP address
+   // Note: The new IP address may be shorter, character-wise, than its default
+   // address, so clear the CLS line before displaying the new one.
    memset(cls_message, (int)' ', CLS_LINE_SIZE);
    cls_message[CLS_LINE_SIZE - 1] = 0;
    myI2CWriteToLine(I2C2, cls_message, 2);
@@ -180,79 +272,106 @@ int main(void)
    // NOW begin your custom code
    while (1)
    {
-      PORTToggleBits(IOPORT_B, BIT_10);
+      add_function_to_queue(blink_onboard_LED_1);
+      //PORTToggleBits(IOPORT_B, BIT_10);
+
+      add_function_to_queue(my_TCPIP_receive_and_reply);
 
       // execute any functions that may have been added by the timer interrupt
+      start_ms = gMillisecondsInOperation;
       execute_functions_in_queue();
+      end_ms = gMillisecondsInOperation;
+      diff_ms = (end_ms - start_ms);
 
-      // I like to blink different LEDs depending on whether there is a
-      // connection or not, and I can get away with it because I am only using
-      // a single socket-port
-      // Note: Blinking LEDs would be a poor way to check the connection status
-      // of a multiple socket-port setup.
-      if (1 == TCPIP_is_there_a_connection_on_port(SERVER_PORT))
+      sum_diff_ms += diff_ms;
+      diff_count += 1;
+      avg_diff_ms = sum_diff_ms / diff_count;
+
+      if (diff_ms < min_diff_ms)
       {
-         PORTClearBits(IOPORT_B, BIT_12);
-         PORTToggleBits(IOPORT_B, BIT_13);
+         min_diff_ms = diff_ms;
       }
-      else
+      if (diff_ms > max_diff_ms)
       {
-         PORTClearBits(IOPORT_B, BIT_13);
-         PORTToggleBits(IOPORT_B, BIT_12);
+         max_diff_ms = diff_ms;
+         ms_count_at_max = gMillisecondsInOperation;
       }
 
-      byte_count = TCPIP_bytes_in_RX_FIFO(SERVER_PORT);
-      if (byte_count > 0)
-      {
-//         myI2CWriteToLine(I2C2, "data Data DATA!", 1);
-//         myI2CWriteToLine(I2C2, "bats Bats BATS!", 2);
+//      snprintf(cls_message, CLS_LINE_SIZE, "%max=%d at=%d", max_diff_ms, ms_count_at_max);
+//      myI2CWriteToLine(I2C2, cls_message, 1);
+//      snprintf(cls_message, CLS_LINE_SIZE, "curr=%d, avg=%d", diff_ms, avg_diff_ms);
+//      myI2CWriteToLine(I2C2, cls_message, 2);
 
-         // missige for you, sire
-         snprintf(cls_message, CLS_LINE_SIZE, "RX bytes: %d", byte_count);
-         myI2CWriteToLine(I2C2, cls_message, 1);
 
-         // clear out the message buffer, get the missige, then display it on
-         // the CLS pmod, line 2 (because I want "missige sire:" to display on
-         // the top line)
-         // Note: If the message is longer than a single CLS line, that is ok.
-         // It will just start overwriting the last character until the string
-         // is finished.
-         memset(rx_buffer, 0, MESSAGE_BUFFER_SIZE_BYTES);
-         byte_count = TCPIP_basic_receive(SERVER_PORT, rx_buffer, MESSAGE_BUFFER_SIZE_BYTES);
-         if (byte_count > CLS_LINE_SIZE)
-         {
-            // ??do something if it is too long to print on a single CLS line? no? do we care??
-         }
-         else if (byte_count == MESSAGE_BUFFER_SIZE_BYTES)
-         {
-            // do something?
-         }
 
-         // forcefully null terminate the receive buffer to make sure that
-         // formated string (%s) procede without incident
-         // Note: Chop off the last two bytes, which are the return carriage
-         // and new line characters that Tera Term sends when the return key is
-         // pressed.
-         // Note: If not using Tera Term, do not chop off the last two bytes
-         // unless you know that the sending program appends unwanted
-         // characters or other bytes to the end.
-         //rx_buffer[byte_count - 2] = 0;
-         rx_buffer[byte_count] = 0;
-         snprintf(cls_message, CLS_LINE_SIZE, "%s", rx_buffer);
-         myI2CWriteToLine(I2C2, cls_message, 2);
-
-         // now spit the message back out, formatted to appear visually
-         // different from the sent message
-         // Note: Cross your fingers and hope that the incoming message wasn't 
-         // so big that the new format will be truncated in the transmit 
-         // buffer.
-         memset(tx_buffer, 0, MESSAGE_BUFFER_SIZE_BYTES);
-         snprintf(tx_buffer, MESSAGE_BUFFER_SIZE_BYTES, "\r\n--You sent: '%s'\r\n", rx_buffer);
-         TCPIP_basic_send(SERVER_PORT, tx_buffer, strlen(tx_buffer));
-      }
+//      // I like to blink different LEDs depending on whether there is a
+//      // connection or not, and I can get away with it because I am only using
+//      // a single socket-port
+//      // Note: Blinking LEDs would be a poor way to check the connection status
+//      // of a multiple socket-port setup.
+//      if (1 == TCPIP_is_there_a_connection_on_port(SERVER_PORT))
+//      {
+//         PORTClearBits(IOPORT_B, BIT_12);
+//         PORTToggleBits(IOPORT_B, BIT_13);
+//      }
+//      else
+//      {
+//         PORTClearBits(IOPORT_B, BIT_13);
+//         PORTToggleBits(IOPORT_B, BIT_12);
+//      }
+//
+//      byte_count = TCPIP_bytes_in_RX_FIFO(SERVER_PORT);
+//      if (byte_count > 0)
+//      {
+////         myI2CWriteToLine(I2C2, "data Data DATA!", 1);
+////         myI2CWriteToLine(I2C2, "bats Bats BATS!", 2);
+//
+//         // missige for you, sire
+//         snprintf(cls_message, CLS_LINE_SIZE, "RX bytes: %d", byte_count);
+//         myI2CWriteToLine(I2C2, cls_message, 1);
+//
+//         // clear out the message buffer, get the missige, then display it on
+//         // the CLS pmod, line 2 (because I want "missige sire:" to display on
+//         // the top line)
+//         // Note: If the message is longer than a single CLS line, that is ok.
+//         // It will just start overwriting the last character until the string
+//         // is finished.
+//         memset(rx_buffer, 0, MESSAGE_BUFFER_SIZE_BYTES);
+//         byte_count = TCPIP_basic_receive(SERVER_PORT, rx_buffer, MESSAGE_BUFFER_SIZE_BYTES);
+//         if (byte_count > CLS_LINE_SIZE)
+//         {
+//            // ??do something if it is too long to print on a single CLS line? no? do we care??
+//         }
+//         else if (byte_count == MESSAGE_BUFFER_SIZE_BYTES)
+//         {
+//            // do something?
+//         }
+//
+//         // forcefully null terminate the receive buffer to make sure that
+//         // formated string (%s) procede without incident
+//         // Note: Chop off the last two bytes, which are the return carriage
+//         // and new line characters that Tera Term sends when the return key is
+//         // pressed.
+//         // Note: If not using Tera Term, do not chop off the last two bytes
+//         // unless you know that the sending program appends unwanted
+//         // characters or other bytes to the end.
+//         //rx_buffer[byte_count - 2] = 0;
+//         rx_buffer[byte_count] = 0;
+//         snprintf(cls_message, CLS_LINE_SIZE, "%s", rx_buffer);
+//         myI2CWriteToLine(I2C2, cls_message, 2);
+//
+//         // now spit the message back out, formatted to appear visually
+//         // different from the sent message
+//         // Note: Cross your fingers and hope that the incoming message wasn't
+//         // so big that the new format will be truncated in the transmit
+//         // buffer.
+//         memset(tx_buffer, 0, MESSAGE_BUFFER_SIZE_BYTES);
+//         snprintf(tx_buffer, MESSAGE_BUFFER_SIZE_BYTES, "\r\n--You sent: '%s'\r\n", rx_buffer);
+//         TCPIP_basic_send(SERVER_PORT, tx_buffer, strlen(tx_buffer));
+//      }
 
       // delay for a little bit so that the LED doesn't blink too fast to see
-      delayMS(50);
+      //delayMS(50);
    }
 
    return 0;
